6488167fc92851e3f08f960f543e3f1746d56fad
Rafoluis <42683667+Rafoluis@users.noreply.github.com>
2018-09-29 15:19:38 -0500

    Update Try_this06.cpp

--- lab2/Try_this06.cpp
+++ lab2/Try_this06.cpp
@@ -5,22 +5,27 @@
 using namespace std;
 
 int main (){
-  string a = "a";
-	string c = "carajo";
-  string m = "mierda";
+	string a = "a";
+	string c = "carajo";	
+	string m = "mierda";
 	string s = "Piiii";
+	vector <string>malas;
 	vector <string>palabras;
 	string palabra;
+	malas.push_back(c);
+	malas.push_back(m);
 	while (palabra != a){
 		cin>>palabra;
 		palabras.push_back(palabra);
 	}
 	for (int i=0;i<palabras.size();i++){
-		if (palabras[i]==c||palabras[i]==m){
-				palabras[i]=s;
+		for(int x=0; x<malas.size();x++){
+			if (palabras[i]==malas[x]){
+				palabras[i]=s;			
 			}
 		}
+  }
   for (int i = 0;i<palabras.size();i++){
-    cout<<palabras[i]<< "_";
+    cout<<palabras[i]<< " ";
   }
 }
